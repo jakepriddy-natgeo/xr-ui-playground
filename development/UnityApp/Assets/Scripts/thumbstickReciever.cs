d0de819d3ceb1c7dc211554092b1e217918f7b68
Jacob Priddy <jacobpriddy@Jacobs-MBP-2.home>
2018-09-05 13:48:40 -0400

    v1.1 --> running proper animation and navigation complete moving to v1.2 ----- v1.2 starting scripting the thumbstick and thumbstick reciever scripts

---
+++ development/UnityApp/Assets/Scripts/thumbstickReciever.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class thumbstickReciever : MonoBehaviour
+{
+
+    public UnityEvent onThumbstickEnter;
+    public UnityEvent onThumbstickStay;
+    public UnityEvent onThumbstickExit;
+    public UnityEvent onThumbsitckRelease;
+
+	private void Start()
+	{
+		
+	}
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+
+        if (col.collider.gameObject.tag == "thumbstick")
+        {
+            col.collider.GetComponent<thumbstickControl>().collision = true;
+            if (!col.collider.GetComponent<thumbstickControl>().dragging)
+            {
+                col.collider.gameObject.transform.position = transform.position;
+                //do something when the thumbstick is dragged onto this object
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+	{
+        col.collider.GetComponent<thumbstickControl>().collision = false;
+	}
+
+}
+
